daf0397d739514b6bd1ea3de6a1534c6743c89eb
Chris Griffith <chris.griffith@gmail.com>
2018-11-17 14:05:21 -0800

    No API keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyB6C6JDo8n0dg9lG1H3-rz1DuIr_AcAe8U',
-    authDomain: 'ionic2do-5b82b.firebaseapp.com',
-    databaseURL: 'https://ionic2do-5b82b.firebaseio.com',
-    projectId: 'ionic2do-5b82b',
-    storageBucket: 'ionic2do-5b82b.appspot.com',
-    messagingSenderId: '583778457575'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
   }
 };
